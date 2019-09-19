41f331651986d9a458a73b73c20cb74dec9ad799
Nihal Singh <nihal.111@gmail.com>
2019-09-18 22:02:47 -0400

    Code cleanup (lint) + added gitignore

--- train.py
+++ train.py
@@ -1,73 +1,75 @@
-from utils import build_vocab,batchIter,idData,isEdit
-from preprocessPTB import readData,preProcess
+from utils import build_vocab, batchIter, idData, isEdit
+from preprocessPTB import readData, preProcess
 import torch
 import torch.nn as nn
 import time
-from torch import optim
 import torch.nn.functional as F
 
 WORD_EMBEDDING_DIM = 64
 INPUT_DIM = 100
 HIDDEN_DIM = 256
-PRINT_EVERY=1000
-EVALUATE_EVERY_EPOCH=1
-ENCODER_LAYER=2
-DROUPOUT_RATE=0.1
-BATCH_SIZE=32
-INIT_LEARNING_RATE=0.0
-EPOCH=400
+PRINT_EVERY = 1000
+EVALUATE_EVERY_EPOCH = 1
+ENCODER_LAYER = 2
+DROUPOUT_RATE = 0.1
+BATCH_SIZE = 32
+INIT_LEARNING_RATE = 0.0
+EPOCH = 400
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 class Encoder(nn.Module):
-    def __init__(self,word_size,word_dim,input_dim,hidden_dim,nLayers,labelSize,dropout_p):
+    def __init__(self, word_size, word_dim, input_dim, hidden_dim,
+                 nLayers, labelSize, dropout_p):
         super(Encoder, self).__init__()
-        self.nLayers=nLayers
-        self.hidden_dim=hidden_dim
-        self.input_dim=input_dim
+        self.nLayers = nLayers
+        self.hidden_dim = hidden_dim
+        self.input_dim = input_dim
 
         self.word_embeds = nn.Embedding(word_size, word_dim)
         self.embeds2input = nn.Linear(word_dim, self.input_dim)
         self.tanh1 = nn.ReLU()
-        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers=self.nLayers, bidirectional=True)
-        self.hidden2output1 = nn.Linear(hidden_dim*2,hidden_dim)
+        self.lstm = nn.LSTM(input_dim, hidden_dim,
+                            num_layers=self.nLayers, bidirectional=True)
+        self.hidden2output1 = nn.Linear(hidden_dim * 2, hidden_dim)
         self.tanh2 = nn.ReLU()
         self.hidden2output2 = nn.Linear(hidden_dim, labelSize)
 
         self.dropout1 = nn.Dropout(dropout_p)
         self.dropout2 = nn.Dropout(dropout_p)
 
-    def forward(self,input,lengths, train=True,hidden=None):
-        input=self.tanh1(self.embeds2input(self.word_embeds(input)))
+    def forward(self, input, lengths, train=True, hidden=None):
+        input = self.tanh1(self.embeds2input(self.word_embeds(input)))
         if train:
-            input=self.dropout1(input)
-        input=input.transpose(0,1)
-        input=nn.utils.rnn.pack_padded_sequence(input, lengths)
+            input = self.dropout1(input)
+        input = input.transpose(0, 1)
+        input = nn.utils.rnn.pack_padded_sequence(input, lengths)
         output, hidden = self.lstm(input, hidden)
         output, _ = nn.utils.rnn.pad_packed_sequence(output)
         if train:
-            output=self.dropout2(output)
-        output=output.transpose(0,1)
-        output=self.hidden2output2(self.tanh2(self.hidden2output1(output)))
-        output=F.log_softmax(output, dim=-1)
+            output = self.dropout2(output)
+        output = output.transpose(0, 1)
+        output = self.hidden2output2(self.tanh2(self.hidden2output1(output)))
+        output = F.log_softmax(output, dim=-1)
         return output
 
 
 class SimpleLossCompute:
     "A simple loss compute and train function."
 
-    def __init__(self,  criterion, opt=None):
+    def __init__(self, criterion, opt=None):
         self.criterion = criterion
         self.opt = opt
 
-    def __call__(self, x, y, norm,train=True):
+    def __call__(self, x, y, norm, train=True):
         loss = self.criterion(x.contiguous().view(-1, x.size(-1)),
                               y.contiguous().view(-1)) / norm
         if train:
             loss.backward()
             if self.opt is not None:
-                #print(loss)
+                # print(loss)
                 self.opt.step()
                 self.opt.optimizer.zero_grad()
         else:
@@ -83,7 +85,7 @@ class LabelSmoothing(nn.Module):
     def __init__(self, size, padding_idx, smoothing=0.0):
         super(LabelSmoothing, self).__init__()
         self.criterion = nn.KLDivLoss(size_average=False)
-        self.padding_idx=padding_idx
+        self.padding_idx = padding_idx
         self.confidence = 1.0 - smoothing
         self.smoothing = smoothing
         self.size = size
@@ -94,16 +96,17 @@ class LabelSmoothing(nn.Module):
         true_dist = x.clone().detach()
         true_dist.fill_(self.smoothing / (self.size - 1))
 
-        true_dist.scatter_(1, target.masked_fill(target == self.padding_idx,0).unsqueeze(1), self.confidence)
+        true_dist.scatter_(1, target.masked_fill(
+            target == self.padding_idx, 0).unsqueeze(1), self.confidence)
         mask = torch.nonzero(target == self.padding_idx)
-        #print(mask.squeeze().size(),true_dist.size())
+        # print(mask.squeeze().size(),true_dist.size())
         if mask.dim() > 0:
             true_dist.index_fill_(0, mask.squeeze(), 0.0)
         self.true_dist = true_dist
         return self.criterion(x, true_dist)
 
-def run_epoch(data_iter, model, loss_compute,train=True,id2label=None):
 
+def run_epoch(data_iter, model, loss_compute, train=True, id2label=None):
     "Standard Training and Logging Function"
     start = time.time()
     total_tokens = 0
@@ -113,49 +116,49 @@ def run_epoch(data_iter, model, loss_compute,train=True,id2label=None):
     editTrueTotal = 0
     editPredTotal = 0
     editCorrectTotal = 0
-    for i, (sent_batch,tag_batch) in enumerate(data_iter):##
-        out = model(sent_batch[0], sent_batch[1],train=train)
-        loss = loss_compute(out, tag_batch[0], sent_batch[2],train=train)
+    for i, (sent_batch, tag_batch) in enumerate(data_iter):
+        out = model(sent_batch[0], sent_batch[1], train=train)
+        loss = loss_compute(out, tag_batch[0], sent_batch[2], train=train)
         total_loss += loss
         total_tokens += sent_batch[2]
         tokens += sent_batch[2]
 
-        if i %  PRINT_EVERY== 0:
+        if i % PRINT_EVERY == 0:
             elapsed = time.time() - start
             print("Epoch Step: %d Tokens per Sec: %f Loss: %f " %
-                    (i, tokens / elapsed , loss / sent_batch[2] ))
+                  (i, tokens / elapsed, loss / sent_batch[2]))
             start = time.time()
             tokens = 0
         if not train:
-            pad=out.size(-1)
+            pad = out.size(-1)
             _, results = torch.max(out.contiguous().view(-1, out.size(-1)), 1)
             results = results.detach().tolist()
             y = tag_batch[0].contiguous().view(-1).detach().tolist()
 
-            for pred,gold in zip(results,y):
-                if not gold==pad:
-                    if isEdit(pred,id2label) and isEdit(gold,id2label):
-                        editCorrectTotal+=1
-                        editTrueTotal+=1
-                        editPredTotal+=1
+            for pred, gold in zip(results, y):
+                if not gold == pad:
+                    if isEdit(pred, id2label) and isEdit(gold, id2label):
+                        editCorrectTotal += 1
+                        editTrueTotal += 1
+                        editPredTotal += 1
                     else:
-                        if isEdit(pred,id2label):
+                        if isEdit(pred, id2label):
                             editPredTotal += 1
-                        if isEdit(gold,id2label):
-                            editTrueTotal +=1
-    f=0.0
+                        if isEdit(gold, id2label):
+                            editTrueTotal += 1
+    f = 0.0
     if not train:
         if not editPredTotal:
             editPredTotal = 1
-            editCorrectTotal=1
+            editCorrectTotal = 1
         if not editCorrectTotal:
-            editCorrectTotal=1
+            editCorrectTotal = 1
         p = editCorrectTotal / editPredTotal
         r = editCorrectTotal / editTrueTotal
-        f=2 * p * r / (p + r)
+        f = 2 * p * r / (p + r)
         print("Edit word precision: %f recall: %f fscore: %f" % (p, r, f))
 
-    return total_loss / total_tokens,f
+    return total_loss / total_tokens, f
 
 
 class NoamOpt:
@@ -183,43 +186,55 @@ class NoamOpt:
         if step is None:
             step = self._step
         return self.factor * \
-               (self.model_size ** (-0.5) *
-                min(step ** (-0.5), step * self.warmup ** (-1.5)))
+            (self.model_size ** (-0.5) *
+             min(step ** (-0.5), step * self.warmup ** (-1.5)))
 
-def run(epoch,model,batch_size,trainData,valData,testData,id2label):
-    valResult=[]
-    testResult=[]
-    criterion = LabelSmoothing(size=len(id2label), padding_idx=len(id2label), smoothing=0.0)
+
+def run(epoch, model, batch_size, trainData, valData, testData, id2label):
+    valResult = []
+    testResult = []
+    criterion = LabelSmoothing(
+        size=len(id2label), padding_idx=len(id2label), smoothing=0.0)
     model_opt = NoamOpt(HIDDEN_DIM, 1, 400,
-                        torch.optim.Adam(model.parameters(), lr=INIT_LEARNING_RATE,betas=(0.9, 0.98), eps=1e-9))
+                        torch.optim.Adam(model.parameters(),
+                                         lr=INIT_LEARNING_RATE,
+                                         betas=(0.9, 0.98), eps=1e-9))
     for i in range(epoch):
         model.train()
-        run_epoch(batchIter(trainData,batch_size,tag_pad=len(id2label)), model,
-                  SimpleLossCompute( criterion, model_opt),train=True)
+        run_epoch(batchIter(trainData, batch_size, tag_pad=len(id2label)),
+                  model, SimpleLossCompute(criterion, model_opt), train=True)
         model.eval()
         print('Evaluation_val: epoch: %d' % (i))
-        loss,f=run_epoch(batchIter(valData, batch_size, tag_pad=len(id2label)), model,
-                  SimpleLossCompute(criterion, model_opt), train=False, id2label=id2label)
+        loss, f = run_epoch(
+            batchIter(valData, batch_size, tag_pad=len(id2label)),
+            model, SimpleLossCompute(criterion, model_opt),
+            train=False, id2label=id2label)
         print('Loss:', loss)
         valResult.append(f)
         print('Evaluation_test: epoch: %d' % (i))
-        loss,f=run_epoch(batchIter(testData, batch_size, tag_pad=len(id2label)), model,
-                                           SimpleLossCompute(criterion, model_opt), train=False, id2label=id2label)
+        loss, f = run_epoch(
+            batchIter(testData, batch_size, tag_pad=len(id2label)),
+            model, SimpleLossCompute(criterion, model_opt),
+            train=False, id2label=id2label)
         print('Loss:', loss)
         testResult.append(f)
-    valBest=max(valResult)
-    print('ValBest epoch:', [i for i, j in enumerate(valResult) if j == valBest])
+    valBest = max(valResult)
+    print('ValBest epoch:', [
+          i for i, j in enumerate(valResult) if j == valBest])
     testBest = max(testResult)
-    print('TestBest epoch:', [i for i, j in enumerate(testResult) if j == testBest])
+    print('TestBest epoch:', [
+          i for i, j in enumerate(testResult) if j == testBest])
 
 
-trainSents=preProcess(readData('dps/swbd/train'))
-valSents=preProcess(readData('dps/swbd/val'))
-testSents=preProcess(readData('dps/swbd/test'))
-vocDic,label2id,id2label=build_vocab(trainSents)
+trainSents = preProcess(readData('dps/swbd/train'))
+valSents = preProcess(readData('dps/swbd/val'))
+testSents = preProcess(readData('dps/swbd/test'))
+vocDic, label2id, id2label = build_vocab(trainSents)
 print(id2label)
-encoder=Encoder(len(vocDic),WORD_EMBEDDING_DIM,INPUT_DIM,HIDDEN_DIM,ENCODER_LAYER,len(id2label),DROUPOUT_RATE).to(device)
-trainData=idData(trainSents,vocDic,label2id)
-valData=idData(valSents,vocDic,label2id)
-testData=idData(testSents,vocDic,label2id)
-run(EPOCH,encoder,BATCH_SIZE,trainData,valData,testData,id2label)
+encoder = Encoder(len(vocDic), WORD_EMBEDDING_DIM, INPUT_DIM,
+                  HIDDEN_DIM, ENCODER_LAYER, len(id2label),
+                  DROUPOUT_RATE).to(device)
+trainData = idData(trainSents, vocDic, label2id)
+valData = idData(valSents, vocDic, label2id)
+testData = idData(testSents, vocDic, label2id)
+run(EPOCH, encoder, BATCH_SIZE, trainData, valData, testData, id2label)
--- utils.py
+++ utils.py
@@ -4,142 +4,156 @@ import torch
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 def readData(dir_name):
     sentList = []
     sent = []
     label = set()
     for file in os.listdir(dir_name):
-        with open(dir_name+'/'+file, 'r') as reader:
+        with open(dir_name + '/' + file, 'r') as reader:
             for line in reader:
                 content = line.strip()
                 if content == '':
                     sentList.append(sent)
                     sent = []
                 else:
-                    tokens=content.split()
+                    tokens = content.split()
                     label.add(tokens[6])
-                    sent.append([tokens[2],tokens[3], tokens[6]])
+                    sent.append([tokens[2], tokens[3], tokens[6]])
         if len(sent) > 0:
             sentList.append(sent)
             sent = []
 
     return sentList
 
+
 def preProcess(sents):
-    newSents=[]
+    newSents = []
     for sent in sents:
-        sent1=[]
-        sent2=[]
+        sent1 = []
+        sent2 = []
         for t in sent:
-            if not t[0].endswith('-') and not t[0]=='SILENCE' and not t[0]=='TRACE' and not t[1]=='None':
+            if not t[0].endswith('-') and not t[0] == 'SILENCE' and \
+                    not t[0] == 'TRACE' and not t[1] == 'None':
                 sent1.append(t)
-        if len(sent1)<=0:
+        if len(sent1) <= 0:
             continue
         tag = 0
-        for (i,t) in enumerate(sent1):
-            if t[2]=='+':
+        for (i, t) in enumerate(sent1):
+            if t[2] == '+':
                 tag = 1
-                if i==0 or not sent1[i-1][2]=='+':
-                    if i==len(sent1)-1 or not sent1[i+1][2]=='+':
+                if i == 0 or not sent1[i - 1][2] == '+':
+                    if i == len(sent1) - 1 or not sent1[i + 1][2] == '+':
                         sent2.append([t[0], t[1], 'BE_IP'])
                     else:
-                        sent2.append([t[0],t[1],'BE'])
-                elif i==len(sent1)-1 or not sent1[i+1][2]=='+':
+                        sent2.append([t[0], t[1], 'BE'])
+                elif i == len(sent1) - 1 or not sent1[i + 1][2] == '+':
                     sent2.append([t[0], t[1], 'IP'])
                 else:
                     sent2.append([t[0], t[1], 'IE'])
-            elif t[2]=='-':
+            elif t[2] == '-':
                 sent2.append([t[0], t[1], 'OR'])
             else:
-                assert(t[2]=='None')
+                assert(t[2] == 'None')
                 sent2.append([t[0], t[1], 'O'])
-        if tag==1:
+        if tag == 1:
             newSents.append(sent2)
     return newSents
 
+
 def stat(sents):
-    difsents=0
-    muti=0
-    nomo=0
+    difsents = 0
+    muti = 0
+    nomo = 0
     for sent in sents:
-        tag=0
+        tag = 0
         for t in sent:
-            if t[2]=='BE':
-                muti+=1
-                tag=1
-            if t[2]=='BE_IP':
-                nomo+=1
-                tag=1
-        if tag==1:
-            difsents+=1
-    return muti,nomo,muti+nomo,difsents
+            if t[2] == 'BE':
+                muti += 1
+                tag = 1
+            if t[2] == 'BE_IP':
+                nomo += 1
+                tag = 1
+        if tag == 1:
+            difsents += 1
+    return muti, nomo, muti + nomo, difsents
 
 
-
-def writeSents(sents,file):
-    with open(file,'w') as writer:
+def writeSents(sents, file):
+    with open(file, 'w') as writer:
         for sent in sents:
             for seq in list(zip(*sent)):
-                writer.write('\t'.join(seq)+'\n')
+                writer.write('\t'.join(seq) + '\n')
             writer.write('\n')
 
-def build_vocab(sents,min_count=0):###
-    vocDic={'<PAD>':0,'<UNK>':1}
-    labels=set()
+
+def build_vocab(sents, min_count=0):
+    vocDic = {'<PAD>': 0, '<UNK>': 1}
+    labels = set()
     for sent in sents:
         for token in sent:
             if not token[0] in vocDic:
-                id=len(vocDic)
-                vocDic[token[0]]=id
+                id = len(vocDic)
+                vocDic[token[0]] = id
                 labels.add(token[2])
-    id2label=list(labels)
-    label2id={}
-    for i,label in enumerate(id2label):
-        label2id[label]=i
+    id2label = list(labels)
+    label2id = {}
+    for i, label in enumerate(id2label):
+        label2id[label] = i
 
-    return vocDic,label2id,id2label
+    return vocDic, label2id, id2label
 
-def get_idx(word,vocDic,unk=1):
+
+def get_idx(word, vocDic, unk=1):
     if word in vocDic:
         return vocDic[word]
     else:
         return unk
 
-def idData(sents,vocDic,tagDic):
-    sents=sorted(sents,key=lambda x:len(x),reverse=True)
-    allSents=[]
-    allTags=[]
+
+def idData(sents, vocDic, tagDic):
+    sents = sorted(sents, key=lambda x: len(x), reverse=True)
+    allSents = []
+    allTags = []
     for sent in sents:
-        if len(sent)<=0:
+        if len(sent) <= 0:
             print(sent)
             continue
-        allSents.append([get_idx(token[0],vocDic) for token in sent])
+        allSents.append([get_idx(token[0], vocDic) for token in sent])
         allTags.append([tagDic[token[2]] for token in sent])
-    return [allSents,allTags]
+    return [allSents, allTags]
 
-def padding(batch,pad=0):
-    lengths=[len(sent) for sent in batch]
-    ntokens=sum(lengths)
-    maxLen=max(lengths)
+
+def padding(batch, pad=0):
+    lengths = [len(sent) for sent in batch]
+    ntokens = sum(lengths)
+    maxLen = max(lengths)
     for sent in batch:
-        if len(sent) <maxLen:
-            sent+=[pad]*(maxLen-len(sent))
-    return [torch.tensor(batch,dtype=torch.long, device=device),torch.tensor(lengths,dtype=torch.long, device=device),ntokens]
+        if len(sent) < maxLen:
+            sent += [pad] * (maxLen - len(sent))
+    return [torch.tensor(batch, dtype=torch.long, device=device),
+            torch.tensor(lengths, dtype=torch.long, device=device), ntokens]
 
-def batchIter(data,batch_size,tag_pad=0,train=True):
-    allSents=data[0]
-    allTags=data[1]
-    nbatch=len(allSents)//batch_size
+
+def batchIter(data, batch_size, tag_pad=0, train=True):
+    allSents = data[0]
+    allTags = data[1]
+    nbatch = len(allSents) // batch_size
     for i in range(nbatch):
-        yield padding(allSents[i*batch_size:(i+1)*batch_size]), padding(allTags[i*batch_size:(i+1)*batch_size],pad=tag_pad)
-    if len(allSents)>nbatch*batch_size:
-        yield padding(allSents[nbatch * batch_size:]), padding(allTags[nbatch * batch_size:],pad=tag_pad)
+        yield padding(allSents[i * batch_size:(i + 1) * batch_size]), \
+            padding(allTags[i * batch_size:(i + 1) * batch_size], pad=tag_pad)
+    if len(allSents) > nbatch * batch_size:
+        yield padding(allSents[nbatch * batch_size:]), \
+            padding(allTags[nbatch * batch_size:], pad=tag_pad)
 
-def isEdit(id,id2label):
-    if not id2label[id]=='O' and not id2label[id]=='OR':
+
+def isEdit(id, id2label):
+    if not id2label[id] == 'O' and not id2label[id] == 'OR':
         return True
     else:
         return False
+
+
 '''trainSents=preProcess(readData('train'))
 valSents=preProcess(readData('val'))
 testSents=preProcess(readData('test'))
